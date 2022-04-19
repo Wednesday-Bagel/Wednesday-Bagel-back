9c7711c41524b38323f4fdf1c86b837f82d41ef4
SIOUkoeran <alstnalsgud@gmail.com>
2022-04-19 15:23:35 +0900

    #1 init

---
+++ src/main/java/com/example/bobbti/BobBtiApplication.java
@@ -0,0 +1,13 @@
+package com.example.bobbti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BobBtiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BobBtiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bobbti/BobBtiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bobbti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BobBtiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
