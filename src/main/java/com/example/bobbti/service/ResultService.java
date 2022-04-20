4c691fd379f333164b342e4fd21fe8544bb0eab3
yuriyeom <43941336+yuriyeom@users.noreply.github.com>
2022-04-20 22:08:08 +0900

    #3 fix:: ResultService 수정

--- src/main/java/com/example/bobbti/service/ResultService.java
+++ src/main/java/com/example/bobbti/service/ResultService.java
@@ -17,23 +17,13 @@ public class ResultService {
         this.questionResultRepository = questionResultRepository;
     }
 
-    // 입력받은 1~12번으로 결과찾아서 ID 리턴
+    // 입력받은 1~12번으로 결과찾아서 dto 리턴
     public ResultDto createUserInput(UserInputDto userInputDto) {
-        // a1~a12로 결과id 찾기
+
         QuestionResult questionResult = this.questionResultRepository.findByAnswer1AndAndAnswer2AndAndAnswer3AndAndAnswer4AndAndAnswer5AndAndAnswer6AndAndAnswer7AndAndAnswer8AndAndAnswer9AndAnswer10AndAnswer11AndAnswer12(
                                             userInputDto.getAnswer1(), userInputDto.getAnswer2(), userInputDto.getAnswer3(), userInputDto.getAnswer4(), userInputDto.getAnswer5(), userInputDto.getAnswer6(), userInputDto.getAnswer7(), userInputDto.getAnswer8(), userInputDto.getAnswer9(), userInputDto.getAnswer10(), userInputDto.getAnswer11(), userInputDto.getAnswer12()
                                         ).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
 
         return new ResultDto(questionResult);
-    }
-
-    // 결과 ID로 결과 DTO 리턴
-    public ResultDto readResult(Long resultId){
-        QuestionResult questionResult = this.questionResultRepository.findById(resultId)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
-
-        ResultDto resultDto = new ResultDto(questionResult);
-
-        return resultDto;
     }
 }
